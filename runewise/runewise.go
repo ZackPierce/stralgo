660711de70d00a1e145a8bf5746a6a89c5d76413
Zachary Pierce <zacharypierce@gmail.com>
2013-10-29 14:44:47 -0700

    Updating runewise subpackage functions to accept rune slices rather than raw strings for the sake of clarity.

--- runewise/runewise.go
+++ runewise/runewise.go
@@ -7,16 +7,12 @@ license that can be found in the LICENSE file.
 Package stralgo/runewise implements various string algorithms
 with an emphasis on similarity metrics, implemented with
 support for multi-byte runes.
-
-This bytewise approach is suited for accurate comparisons
-when the encoded data truly makes use of UTF8.
 */
 package runewise
 
 import (
 	"errors"
 	"fmt"
-	"strings"
 	"unicode"
 )
 
@@ -24,23 +20,23 @@ import (
 // two strings containing equal numbers of runes.
 //
 // The Hamming distance is the total number of runes
-// that differ at the same index in the resolved array
-// of runes from each string.
+// that differ at the same index within the two strings.
+//
 // The higher the result, the more different the strings.
 
 // See: http://en.wikipedia.org/wiki/Hamming_distance
 //
 // Returns an error if the string rune counts are not equal.
-func HammingDistance(a, b string) (uint, error) {
-	aRunes, aLen := runeSlice(a)
-	bRunes, bLen := runeSlice(b)
+func HammingDistance(a, b []rune) (uint, error) {
+	aLen := len(a)
+	bLen := len(b)
 
 	if aLen != bLen {
 		return 0, errors.New("Hamming distance is undefined between strings of unequal length.")
 	}
 	var d uint
 	for i := 0; i < aLen; i++ {
-		if aRunes[i] != bRunes[i] {
+		if a[i] != b[i] {
 			d++
 		}
 	}
@@ -64,9 +60,9 @@ func HammingDistance(a, b string) (uint, error) {
 //
 // Returns an error if both of the input strings contain
 // less than two runes.
-func DiceCoefficient(a, b string) (float64, error) {
-	aRunes, aLen := runeSlice(a)
-	bRunes, bLen := runeSlice(b)
+func DiceCoefficient(a, b []rune) (float64, error) {
+	aLen := len(a)
+	bLen := len(b)
 	aLimit := aLen - 1
 	bLimit := bLen - 1
 	if aLimit < 1 && bLimit < 1 {
@@ -76,7 +72,7 @@ func DiceCoefficient(a, b string) (float64, error) {
 	totalBigrams := 0.0
 	var bigram runeBigram
 	for i := 0; i < aLimit; i++ {
-		bigram = runeBigram{rA: aRunes[i], rB: aRunes[i+1]}
+		bigram = runeBigram{rA: a[i], rB: a[i+1]}
 		if !aSet[bigram] {
 			totalBigrams++
 			aSet[bigram] = true
@@ -86,7 +82,7 @@ func DiceCoefficient(a, b string) (float64, error) {
 	bSet := make(map[runeBigram]bool, bLimit)
 	sharedBigrams := 0.0
 	for i := 0; i < bLimit; i++ {
-		bigram = runeBigram{rA: bRunes[i], rB: bRunes[i+1]}
+		bigram = runeBigram{rA: b[i], rB: b[i+1]}
 		if !bSet[bigram] {
 			totalBigrams++
 			bSet[bigram] = true
@@ -114,9 +110,9 @@ func DiceCoefficient(a, b string) (float64, error) {
 //
 // Returns an error if neither of the input strings
 // contains at least one rune bigram without whitespace.
-func WhiteSimilarity(a, b string) (float64, error) {
-	aPairs, aLen := wordLetterPairs(strings.ToUpper(a))
-	bPairs, bLen := wordLetterPairs(strings.ToUpper(b))
+func WhiteSimilarity(a, b []rune) (float64, error) {
+	aPairs, aLen := upperWordLetterPairs(a)
+	bPairs, bLen := upperWordLetterPairs(b)
 	union := aLen + bLen
 	if union == 0 {
 		return 0.0, errors.New("At least one of the input strings must contain two or more non-whitespace rune bigrams in order to calculate the White Similarity.")
@@ -134,9 +130,8 @@ func WhiteSimilarity(a, b string) (float64, error) {
 	return 2 * intersection / float64(union), nil
 }
 
-func wordLetterPairs(s string) ([]runeBigram, int) {
-	runes, n := runeSlice(s)
-	limit := n - 1
+func upperWordLetterPairs(runes []rune) ([]runeBigram, int) {
+	limit := len(runes) - 1
 	if limit < 1 {
 		return make([]runeBigram, 0), 0
 	}
@@ -154,7 +149,7 @@ func wordLetterPairs(s string) ([]runeBigram, int) {
 		if unicode.IsSpace(a) {
 			continue
 		}
-		bigrams[numPairs] = runeBigram{rA: a, rB: b}
+		bigrams[numPairs] = runeBigram{rA: unicode.ToUpper(a), rB: unicode.ToUpper(b)}
 		numPairs++
 	}
 	bigrams = bigrams[0:numPairs]
@@ -172,17 +167,14 @@ func wordLetterPairs(s string) ([]runeBigram, int) {
 // The larger the result, the more different the strings.
 //
 // See: http://en.wikipedia.org/wiki/Levenshtein_distance
-func LevenshteinDistance(a, b string) (int, error) {
-	aRunes, aLen := runeSlice(a)
-	bRunes, bLen := runeSlice(b)
+func LevenshteinDistance(a, b []rune) (int, error) {
+	aLen := len(a)
+	bLen := len(b)
 	if aLen == 0 {
 		return bLen, nil
 	}
 	if bLen == 0 {
 		return aLen, nil
-	}
-	if aLen == bLen && a == b {
-		return 0, nil
 	}
 
 	rowLen := bLen + 1
@@ -195,7 +187,7 @@ func LevenshteinDistance(a, b string) (int, error) {
 	for i := 0; i < aLen; i++ {
 		currRow[0] = i + 1
 		for j := 0; j < bLen; j++ {
-			if aRunes[i] == bRunes[j] {
+			if a[i] == b[j] {
 				cost = 0
 			} else {
 				cost = 1
@@ -208,6 +200,82 @@ func LevenshteinDistance(a, b string) (int, error) {
 		prevRow, currRow = currRow, prevRow
 	}
 	return prevRow[bLen], nil
+}
+
+// DamerauLevenshteinDistance calculates the magnitude
+// of difference between two strings using the Damerau-
+// Levenshtein algorithm with adjacent-only transpositions,
+// runewise.
+//
+// This edit distance is the minimum number of single-rune
+// edits (insertions, deletions, substitutions, or
+// transpositions) to transform one string into the other.
+// DamerauLevenshtein differs from Levenshtein primarily
+// in that DamerauLevenshtein considers adjacent-rune transpositions.
+//
+// The larger the result, the more different the strings.
+//
+// See: http://en.wikipedia.org/wiki/Damerau-Levenshtein_distance
+func DamerauLevenshteinDistance(a, b []rune) (int, error) {
+	aLen := len(a)
+	bLen := len(b)
+	if aLen == 0 {
+		return bLen, nil
+	} else if bLen == 0 {
+		return aLen, nil
+	}
+
+	// Swap to ensure aRunes contains the shorter slice
+	if aLen > bLen {
+		a, aLen, b, bLen = b, bLen, a, aLen
+	}
+	rowLen := aLen + 1
+	tranRow := make([]int, rowLen, rowLen)
+	prevRow := make([]int, rowLen, rowLen)
+	currRow := make([]int, rowLen, rowLen)
+	for h := 0; h < rowLen; h++ {
+		prevRow[h] = h
+	}
+	var prevB rune
+	var cost int
+	for i := 1; i <= bLen; i++ {
+		currB := b[i-1]
+		currRow[0] = i
+
+		start := i - bLen - 1
+		if start < 1 {
+			start = 1
+		}
+		end := i + bLen + 1
+		if end > aLen {
+			end = aLen
+		}
+
+		var prevA rune
+		for j := start; j <= end; j++ {
+			currA := a[j-1]
+			if currA == currB {
+				cost = 0
+			} else {
+				cost = 1
+			}
+			entry := min(
+				currRow[j-1]+1,
+				prevRow[j]+1,
+				prevRow[j-1]+cost)
+			if currA == prevB && currB == prevA {
+				trans := tranRow[j-2] + cost
+				if trans < entry {
+					entry = trans
+				}
+			}
+			currRow[j] = entry
+			prevA = currA
+		}
+		prevB = currB
+		tranRow, prevRow, currRow = prevRow, currRow, tranRow
+	}
+	return prevRow[aLen], nil
 }
 
 func min(a, b, c int) int {
@@ -227,15 +295,4 @@ type runeBigram struct {
 
 func (r runeBigram) String() string {
 	return fmt.Sprintf("{%q, %q}", r.rA, r.rB)
-}
-
-func runeSlice(s string) ([]rune, int) {
-	n := 0
-	runes := make([]rune, len(s))
-	for _, r := range s {
-		runes[n] = r
-		n++
-	}
-	runes = runes[0:n]
-	return runes, n
 }
--- runewise/runewise_test.go
+++ runewise/runewise_test.go
@@ -8,187 +8,206 @@ import (
 )
 
 func Test_HammingDistance(t *testing.T) {
-	d, err := HammingDistance("toned", "roses")
+	d, err := HammingDistance([]rune("toned"), []rune("roses"))
 	assert.Nil(t, err, "HammingDistance should not return an error for two strings of equivalent length.")
 	assert.Equal(t, uint(3), d, "HammingDistance between 'toned' and 'roses' should be 3")
 
-	d, err = HammingDistance("", "")
+	d, err = HammingDistance([]rune(""), []rune(""))
 	assert.Nil(t, err, "HammingDistance should not return an error for two empty strings")
 	assert.Equal(t, uint(0), d, "HammingDistance between two empty strings should be 0")
 
-	d, err = HammingDistance("spam", "spam")
+	d, err = HammingDistance([]rune("spam"), []rune("spam"))
 	assert.Nil(t, err, "HammingDistance should not return an error for two equivalent non-empty strings.")
 	assert.Equal(t, uint(0), d, "HammingDistance between 'spam' and 'spam' should be 0, but was %i", d)
 
-	d, err = HammingDistance("green eggs", "ham")
+	d, err = HammingDistance([]rune("green eggs"), []rune("ham"))
 	assert.NotNil(t, err, "HammingDistance between 'green eggs' and 'ham' should produce an error due to unequal lengths")
 	assert.Equal(t, uint(0), d)
 
-	d, err = HammingDistance("日本語", "日本ゴ")
+	d, err = HammingDistance([]rune("日本語"), []rune("日本ゴ"))
 	assert.Nil(t, err)
 	assert.Equal(t, uint(1), d)
 
-	d, err = HammingDistance("日本語", "日本g")
+	d, err = HammingDistance([]rune("日本語"), []rune("日本g"))
 	assert.Nil(t, err)
 	assert.Equal(t, uint(1), d)
 
-	d, err = HammingDistance("日本語", "日本go")
+	d, err = HammingDistance([]rune("日本語"), []rune("日本go"))
 	assert.NotNil(t, err)
 	assert.Equal(t, uint(0), d)
 }
 
 func Test_DiceCoefficient(t *testing.T) {
-	c, err := DiceCoefficient("night", "nacht")
+	c, err := DiceCoefficient([]rune("night"), []rune("nacht"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0/4.0, c)
 
-	c, err = DiceCoefficient("GGGG", "GGGG")
+	c, err = DiceCoefficient([]rune("GGGG"), []rune("GGGG"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0, c)
 
-	c, err = DiceCoefficient("", "")
+	c, err = DiceCoefficient([]rune(""), []rune(""))
 	assert.NotNil(t, err)
 	assert.Equal(t, 0.0, c)
 
-	c, err = DiceCoefficient("a", "b")
+	c, err = DiceCoefficient([]rune("a"), []rune("b"))
 	assert.NotNil(t, err)
 	assert.Equal(t, 0.0, c)
 
-	c, err = DiceCoefficient("GG", "GGGG")
+	c, err = DiceCoefficient([]rune("GG"), []rune("GGGG"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0, c, "Naive Dice coefficient does not account for differences in occurrence-count for bigrams.")
 
-	c, err = DiceCoefficient("日本語", "日本語")
+	c, err = DiceCoefficient([]rune("日本語"), []rune("日本語"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0, c)
 
-	c, err = DiceCoefficient("日本語", "日本ゴ")
+	c, err = DiceCoefficient([]rune("日本語"), []rune("日本ゴ"))
 	assert.Nil(t, err)
 	assert.Equal(t, 2.0/4.0, c)
 
-	c, err = DiceCoefficient("日本語", "日本g")
+	c, err = DiceCoefficient([]rune("日本語"), []rune("日本g"))
 	assert.Nil(t, err)
 	assert.Equal(t, 2.0/4.0, c)
 
-	c, err = DiceCoefficient("日", "本")
+	c, err = DiceCoefficient([]rune("日"), []rune("本"))
 	assert.NotNil(t, err, "DiceCoefficient runewise reports an error due to lack of rune-bigrams.")
 	assert.Equal(t, 0.0, c)
 
 	// [230 151 165] and [230, 151, 168]
-	c, err = DiceCoefficient("日", "旨")
+	c, err = DiceCoefficient([]rune("日"), []rune("旨"))
 	assert.NotNil(t, err, "DiceCoefficient runewise reports an error due to lack of rune-bigrams.")
 	assert.Equal(t, 0.0, c)
 }
 
 func Test_WhiteSimilarity(t *testing.T) {
-	c, err := WhiteSimilarity("Healed", "Healed")
+	c, err := WhiteSimilarity([]rune("Healed"), []rune("Healed"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0, c)
 
-	c, err = WhiteSimilarity("Healed", "Sealed")
+	c, err = WhiteSimilarity([]rune("Healed"), []rune("Sealed"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.8, c, 0.01, "Sealed")
 
-	c, err = WhiteSimilarity("Healed", "Healthy")
+	c, err = WhiteSimilarity([]rune("Healed"), []rune("Healthy"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.55, c, 0.01, "Healthy")
 
-	c, err = WhiteSimilarity("Healed", "Heard")
+	c, err = WhiteSimilarity([]rune("Healed"), []rune("Heard"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.44, c, 0.01, "Heard")
 
-	c, err = WhiteSimilarity("Healed", "Herded")
+	c, err = WhiteSimilarity([]rune("Healed"), []rune("Herded"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.40, c, 0.01, "Herded")
 
-	c, err = WhiteSimilarity("Healed", "Help")
+	c, err = WhiteSimilarity([]rune("Healed"), []rune("Help"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.25, c, 0.01, "Help")
 
-	c, err = WhiteSimilarity("Healed", "Sold")
+	c, err = WhiteSimilarity([]rune("Healed"), []rune("Sold"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.0, c, 0.01, "Sold")
 
-	c, err = WhiteSimilarity("Healed ", "HEALed")
+	c, err = WhiteSimilarity([]rune("Healed "), []rune("HEALed"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0, c)
 
-	c, err = WhiteSimilarity("GGGG", "GGGG")
+	c, err = WhiteSimilarity([]rune("GGGG"), []rune("GGGG"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1.0, c)
 
-	c, err = WhiteSimilarity("REPUBLIC OF FRANCE", "FRANCE")
+	c, err = WhiteSimilarity([]rune("REPUBLIC OF FRANCE"), []rune("FRANCE"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.56, c, 0.01)
 
-	c, err = WhiteSimilarity("FRANCE", "QUEBEC")
+	c, err = WhiteSimilarity([]rune("FRANCE"), []rune("QUEBEC"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.0, c, 0.001)
 
-	c, err = WhiteSimilarity("FRENCH REPUBLIC", "REPUBLIC OF FRANCE")
+	c, err = WhiteSimilarity([]rune("FRENCH REPUBLIC"), []rune("REPUBLIC OF FRANCE"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.72, c, 0.01)
 
-	c, err = WhiteSimilarity("FRENCH REPUBLIC", "REPUBLIC OF CUBA")
+	c, err = WhiteSimilarity([]rune("FRENCH REPUBLIC"), []rune("REPUBLIC OF CUBA"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.61, c, 0.01)
 
-	c, err = WhiteSimilarity("", "")
+	c, err = WhiteSimilarity([]rune(""), []rune(""))
 	assert.NotNil(t, err)
 	assert.Equal(t, 0.0, c)
 
-	c, err = WhiteSimilarity("a", "b")
+	c, err = WhiteSimilarity([]rune("a"), []rune("b"))
 	assert.NotNil(t, err)
 	assert.Equal(t, 0.0, c)
 
-	c, err = WhiteSimilarity("GG", "GGGGG")
+	c, err = WhiteSimilarity([]rune("GG"), []rune("GGGGG"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.4, c, 0.01)
 
-	c, err = WhiteSimilarity("GGGGG", "GG")
+	c, err = WhiteSimilarity([]rune("GGGGG"), []rune("GG"))
 	assert.Nil(t, err)
 	EqualWithin(t, 0.4, c, 0.01)
 }
 
 func Test_LevenshteinDistance_Easy(t *testing.T) {
-	d, err := LevenshteinDistance("test", "test")
+	d, err := LevenshteinDistance([]rune("test"), []rune("test"))
 	assert.Nil(t, err)
 	assert.Equal(t, 0, d)
 
-	d, err = LevenshteinDistance("test", "tent")
+	d, err = LevenshteinDistance([]rune("test"), []rune("tent"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, d)
 
-	d, err = LevenshteinDistance("gumbo", "gambol")
+	d, err = LevenshteinDistance([]rune("gumbo"), []rune("gambol"))
 	assert.Nil(t, err)
 	assert.Equal(t, 2, d)
 
-	d, err = LevenshteinDistance("kitten", "sitting")
+	d, err = LevenshteinDistance([]rune("kitten"), []rune("sitting"))
 	assert.Nil(t, err)
 	assert.Equal(t, 3, d)
 
-	d, err = LevenshteinDistance("foo", "")
+	d, err = LevenshteinDistance([]rune("foo"), []rune(""))
 	assert.Nil(t, err)
 	assert.Equal(t, 3, d)
 
-	d, err = LevenshteinDistance("", "foo")
+	d, err = LevenshteinDistance([]rune(""), []rune("foo"))
 	assert.Nil(t, err)
 	assert.Equal(t, 3, d)
 
-	d, err = LevenshteinDistance("", "")
+	d, err = LevenshteinDistance([]rune(""), []rune(""))
 	assert.Nil(t, err)
 	assert.Equal(t, 0, d)
 
-	d, err = LevenshteinDistance("a", "")
+	d, err = LevenshteinDistance([]rune("a"), []rune(""))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, d)
 
 }
 
+func Test_DamerauLevenshteinDistance(t *testing.T) {
+	d, err := DamerauLevenshteinDistance([]rune("azertyuiop"), []rune("aeryuop"))
+	assert.Nil(t, err)
+	assert.Equal(t, 3, d)
+
+	d, err = DamerauLevenshteinDistance([]rune("aeryuop"), []rune("azertyuiop"))
+	assert.Nil(t, err)
+	assert.Equal(t, 3, d)
+
+	d, err = DamerauLevenshteinDistance([]rune("azertyuiopqsdfghjklmwxcvbn"), []rune("qwertyuiopasdfghjkl;zxcvbnm"))
+	assert.Nil(t, err)
+	assert.Equal(t, 6, d)
+
+	d, err = DamerauLevenshteinDistance([]rune("1234567890"), []rune("1324576809"))
+	assert.Nil(t, err)
+	assert.Equal(t, 3, d)
+
+}
+
 func Benchmark_LevenshteinDistance(b *testing.B) {
-	for i := 0; i < b.N ; i++ {
-		LevenshteinDistance("kitten", "sitting")
-		LevenshteinDistance("gumbo", "gambol")
+	for i := 0; i < b.N; i++ {
+		LevenshteinDistance([]rune("kitten"), []rune("sitting"))
+		LevenshteinDistance([]rune("gumbo"), []rune("gambol"))
 	}
 }
 
